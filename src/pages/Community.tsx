a53c445326b3261ac31919a4296b6015056859a3
Samuel Oyenuga <psa8.sa@gmail.com>
2025-05-13 21:33:40 +0100

    Nintyninth commit

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -338,7 +338,7 @@ const Community = () => {
                       <img
                         src={Rocket}
                         alt="Rocket"
-                        className="w-60 h-60 absolute top-1/2 left-1/2 -translate-x-4/10 -translate-y-45/100"
+                        className="w-60 h-60 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
                         onError={(e) => {
                           e.currentTarget.style.display = "none";
                         }}
