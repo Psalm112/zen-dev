f7a4778d38f6ca6c73be177c0584ef64480c08a6
Samuel Oyenuga <psa8.sa@gmail.com>
2025-05-22 16:44:59 +0100

    first commit

--- src/components/common/FloatingActionButton.tsx
+++ src/components/common/FloatingActionButton.tsx
@@ -18,7 +18,7 @@ const FloatingActionButton: FC<FloatingActionButtonProps> = ({
   color = "primary",
 }) => {
   const positionClasses = {
-    "bottom-right": "bottom-30 md:bottom-20 max-xl:right-2",
+    "bottom-right": "bottom-50 md:bottom-20 max-xl:right-2 right-0",
     "bottom-center": "bottom-20 left-1/2 transform -translate-x-1/2",
   };
 
