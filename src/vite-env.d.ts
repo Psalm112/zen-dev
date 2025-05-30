f02c3da1233f41cec8f686a6a6f549e55a1c45e9
Samuel Oyenuga <psa8.sa@gmail.com>
2025-05-30 22:19:58 +0100

    final commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,7 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_ESCROW_CONTRACT_ADDRESS: string;
   readonly VITE_USDT_CONTRACT_ADDRESS: string;
   readonly VITE_WALLETCONNECT_PROJECT_ID: string;
-  // readonly VITE_INFURA_KEY: string;
+  readonly VITE_INFURA_KEY: string;
   readonly VITE_APP_NAME: string;
   readonly VITE_APP_VERSION: string;
 }
